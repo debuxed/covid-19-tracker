fd2defcab6859ea18f3877f1f76c3e120d60e5ce
unknown <itsdexu5@gmail.com>
2020-09-24 11:06:05 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from './components';
+import { fetchData } from './api'; // the api just has index.js so we can just specify the ./api to call index.js
+import styles from './App.module.css'
+class App extends React.Component {
+    // create the state to track the data
+    // we can even use constructor to initialize the data
+    state = {
+        data: {},
+    }
+    async componentDidMount() {
+        // populating the fetcheData element which is called using api
+        const fetchedData = await fetchData();
+
+        // set the state and pass the fetchedData as the data which is then passed to the prop
+        this.setState({data: fetchedData});
+    }
+
+    render() {
+        const { data } = this.state;
+            
+        return(
+            // destructure the value of data from state 
+            
+            <div className={styles.container}>
+                <Cards data={ data }/>
+                <CountryPicker />
+                <Chart />
+            </div>
+        )
+    }
 }
 
-export default App;
+
+export default App; 
